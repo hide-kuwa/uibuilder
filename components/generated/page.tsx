c7ebb65b2c1f534e78b2bcd02e1020cf17eec708
hide-kuwa <hideyuki.kuwayama@gmai.com>
2025-08-26 11:36:04 +0900

    依存関係修正

---
+++ components/generated/page.tsx
@@ -0,0 +1,12 @@
+import { div, TextBlock, Button } from "@/components";
+
+export default function Page() {
+  return (
+    <>
+      <div>
+        <TextBlock text="ようこそ！" align="center" />
+      <Button label="次へ" variant="solid" />
+      </div>
+    </>
+  );
+}
