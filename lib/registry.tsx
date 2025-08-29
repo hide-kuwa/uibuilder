795fb75bf71871a4e1205dec3c9df8c97bc901d0
hide-kuwa <hideyuki.kuwayama@gmail.com>
2025-08-29 17:48:47 +0900

    refactor: use static component registry

--- lib/registry.tsx
+++ lib/registry.tsx
@@ -1,13 +1,10 @@
 import meta from '../component-meta.json'
 import type { ComponentType } from 'react'
+import MyCard from '../src/components/custom/MyCard'
 
 export const library = meta as { displayName: string; description?: string }[]
 
-export const components: Record<string, ComponentType<any>> = {}
-for (const m of library) {
-  components[m.displayName] = (props: any) => {
-    const React = require('react')
-    const Mod = require(`../src/components/custom/${m.displayName}.tsx`).default
-    return React.createElement(Mod, props)
-  }
+// Static registry of available components
+export const components: Record<string, ComponentType<any>> = {
+  MyCard,
 }
