2be359b83ded5b7874da567d5a8bdcc4e5f49ed0
hide-kuwa <hideyuki.kuwayama@gmail.com>
2025-09-02 23:27:41 +0900

    feat: add AnimePing component for anime.js smoke test

---
+++ web/components/AnimePing.tsx
@@ -0,0 +1,18 @@
+'use client'
+const animeP = () => import('animejs').then(m => m.default)
+
+export default function AnimePing() {
+  return (
+    <button
+      className="rounded-lg bg-white/10 px-3 py-2"
+      onClick={async (e) => {
+        const anime = await animeP()
+        const el = e.currentTarget
+        anime.remove(el)
+        anime({ targets: el, scale: [1, 1.08, 1], duration: 280, easing: 'easeInOutQuad' })
+      }}
+    >
+      ping
+    </button>
+  )
+}
