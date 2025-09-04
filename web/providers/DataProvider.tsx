5461cce539b6dcaf5bc5ac275c55b8adece3b44b
hide-kuwa <hideyuki.kuwayama@gmail.com>
2025-09-04 19:28:07 +0900

    feat: add devtools toggle page

--- web/providers/DataProvider.tsx
+++ web/providers/DataProvider.tsx
@@ -1,14 +1,10 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PropsWithChildren, useState } from "react";
 
 export default function DataProvider({ children }: PropsWithChildren) {
   const [client] = useState(() => new QueryClient());
   return (
-    <QueryClientProvider client={client}>
-      {children}
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    <QueryClientProvider client={client}>{children}</QueryClientProvider>
   );
 }
